d7888333c85a5e2697660db4297bb50ebee62a7d
Nicholas Reid <nicholasreid845@gmail.com>
2021-04-15 13:27:14 -0400

    modified readme, added images to resource file

--- src/config.js
+++ src/config.js
@@ -1,3 +1,6 @@
 const API_BASE_URL = "https://blooming-headland-41329.herokuapp.com/api"
 
+// CHANGE TO http://localhost:3000/api FOR DEVELOPMENT 
+// OR WHEREVER YOU MAY BE RUNNING LOCALLY
+
 export default API_BASE_URL
